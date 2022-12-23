9b7393fdfaa4e6d30d68b61acbf1d4051b4f6fdb
Anton Peshekhonov <noviyya2021@gmail.com>
2022-12-23 21:28:43 +0300

    react router

---
+++ src/app/components/posts.jsx
@@ -0,0 +1,23 @@
+import Post from "./post";
+import PostsList from "./postsList";
+
+const Posts = ({match}) => {
+    const posts = [
+        {id:1, label:"post 1"},
+        {id:2, label:"post 2"},
+        {id:3, label:"post 3"},
+      ]
+const postId = match.params.postId
+
+    return (
+        <>
+            {postId ? (
+                <Post posts={posts} id={postId}/>
+            ) : (
+                <PostsList posts={posts} />
+            )}
+        </>
+    )
+}
+ 
+export default Posts;
