8735ffb6cdafb2ad53926db598f86774d39c3efa
Anton Peshekhonov <noviyya2021@gmail.com>
2022-12-25 18:42:43 +0300

    Use hooks

--- src/app/components/posts.jsx
+++ src/app/components/posts.jsx
@@ -1,38 +1,36 @@
-import React, {useEffect, useState} from 'react';
-import _ from 'lodash'
+import React, { useState} from 'react';
 import Post from "./post";
 import PostsList from "./postsList";
-import query from "query-string"
+import {useParams} from "react-router-dom"
 
-const Posts = ({match, location }) => {
-    const [showOffer, setShowOffer] = useState(false)
+const Posts = () => {
+    const params = useParams()
+    // const [showOffer, setShowOffer] = useState(false)
 
     const posts = [
-        {id:1, label:"post 1"},
-        {id:2, label:"post 2"},
-        {id:3, label:"post 3"},
+        {id: 1, label:"post 1"},
+        {id: 2, label:"post 2"},
+        {id: 3, label:"post 3"},
     ]
-    const search = query.parse(location.search)
-    const postId = match.params.postId
+    const {postId} = params
+    console.log(postId)
     // useEffect(() => console.log(search), [search]);
-    const cropPosts = search && search!==""
-        ? _(posts).slice(0).take(search.count).value()
-        : posts
-    const {fromUrl, block} = search
-    useEffect(() => {
-        if (!showOffer && fromUrl && block) {
-            setShowOffer(true)
-        }
-    }, [fromUrl,block,showOffer])
+   
+    // const {fromUrl, block} = search
+    // useEffect(() => {
+    //     if (!showOffer && fromUrl && block) {
+    //         setShowOffer(true)
+    //     }
+    // }, [fromUrl,block,showOffer])
 
 
     return (
         <>
-            {showOffer && <div>Специальное предложение</div>}
+            {/* {showOffer && <div>Специальное предложение</div>} */}
             {postId ? (
-                <Post posts={posts} id={postId}/>
+                <Post posts={posts} id={postId} />
             ) : (
-                <PostsList posts={cropPosts} />
+                <PostsList posts={posts} />
             )}
         </>
     )
