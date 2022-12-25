6fcbffd1903edfe2db7f9df6bf5b24d7ffc897d9
Anton Peshekhonov <noviyya2021@gmail.com>
2022-12-25 11:30:55 +0300

    Add Redirect

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,11 @@
 import NavBar from "./components/navBar";
-import { Route, Switch } from "react-router-dom"
+import { Route, Switch, Redirect } from "react-router-dom"
 import Login from "./components/login";
 import Posts from "./components/posts";
 import Home from "./components/home";
 import Dashboard from "./components/dashboard";
 import Stats from "./components/stats";
+import NotFound from "./components/not-found";
 
 
 function App() {
@@ -17,9 +18,10 @@ function App() {
         <Route path="/dashboard/stats" component={ Stats }  />
         <Route path="/dashboard"  component={ Dashboard }/>
         <Route path="/login" component={ Login }  />
-        <Route
-          path="/posts/:postId?" component={Posts}
-      />
+        <Route path="/posts/:postId?" component={Posts} />
+        <Route path="/404" component={NotFound} />
+        <Redirect to="/404" />
+        <Redirect from="/admin" to="/dashboard" />
       </Switch>
     </div>  
   );
@@ -27,3 +29,4 @@ function App() {
 
 export default App;
 // render={(props) => <Dashboard isAdmin={false} {...props} />}
+// path="/posts/:postId?/:display?" component={Posts}
--- src/app/components/posts.jsx
+++ src/app/components/posts.jsx
@@ -1,20 +1,38 @@
+import React, {useEffect, useState} from 'react';
+import _ from 'lodash'
 import Post from "./post";
 import PostsList from "./postsList";
+import query from "query-string"
 
-const Posts = ({match}) => {
+const Posts = ({match, location }) => {
+    const [showOffer, setShowOffer] = useState(false)
+
     const posts = [
         {id:1, label:"post 1"},
         {id:2, label:"post 2"},
         {id:3, label:"post 3"},
-      ]
-const postId = match.params.postId
+    ]
+    const search = query.parse(location.search)
+    const postId = match.params.postId
+    // useEffect(() => console.log(search), [search]);
+    const cropPosts = search && search!==""
+        ? _(posts).slice(0).take(search.count).value()
+        : posts
+    const {fromUrl, block} = search
+    useEffect(() => {
+        if (!showOffer && fromUrl && block) {
+            setShowOffer(true)
+        }
+    }, [fromUrl,block,showOffer])
+
 
     return (
         <>
+            {showOffer && <div>Специальное предложение</div>}
             {postId ? (
                 <Post posts={posts} id={postId}/>
             ) : (
-                <PostsList posts={posts} />
+                <PostsList posts={cropPosts} />
             )}
         </>
     )
